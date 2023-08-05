3f7a1faac385ea9146af6c12978bee06623157fa
MarcussHa <hathong.work@gmail.com>
2023-08-05 16:36:48 +0700

    fix bug doc

--- departments/models.py
+++ departments/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-
 class Departments(models.Model):
-    departmentid = models.IntegerField(db_column='DepartmentID', primary_key=True)  # Field name made lowercase.
-    named = models.CharField(db_column='NameD', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    departmentid = models.IntegerField(db_column='DepartmentID', primary_key=True)
+    named = models.CharField(db_column='NameD', max_length=50, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'departments'
-# Create your models here.
+
+    def __str__(self):
+        return self.named
