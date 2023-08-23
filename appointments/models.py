6211e636ba546c63adef42b14ed60bdb6883e741
MarcussHa <hathong.work@gmail.com>
2023-08-23 14:36:23 +0700

    complete appointment

--- appointments/models.py
+++ appointments/models.py
@@ -4,14 +4,16 @@ from ministration.models import Ministration
 from doctors.models import Doctors
 
 # Create your models here.
-
 class Appointment(models.Model):
-    appointmentid = models.AutoField(db_column='appointmentID', primary_key=True)  # Field name made lowercase.
-    datebooking = models.DateTimeField(db_column='Datebooking')  # Field name made lowercase.
+    appid = models.AutoField(db_column='AppID', primary_key=True)  # Field name made lowercase.
+    fullname = models.CharField(db_column='Fullname', max_length=70)  # Field name made lowercase.
+    email = models.CharField(db_column='Email', max_length=50)  # Field name made lowercase.
+    phone = models.IntegerField(db_column='Phone')  # Field name made lowercase.
+    datebooking = models.DateField(db_column='Datebooking')  # Field name made lowercase.
+    serviceid = models.ForeignKey(Ministration, on_delete=models.DO_NOTHING, db_column='ServiceID', blank=True, null=True)
+    docid = models.ForeignKey(Doctors, on_delete=models.DO_NOTHING, db_column='DocID', blank=True, null=True) 
     note = models.CharField(db_column='Note', max_length=100, blank=True, null=True)  # Field name made lowercase.
-    customerid = models.ForeignKey(AuthUser, on_delete=models.DO_NOTHING, db_column='CustomerID', blank=True, null=True)
-    serviceid = models.ForeignKey(Ministration, on_delete=models.DO_NOTHING, db_column='ServiceID', blank=True, null=True)  
-    docid = models.ForeignKey(Doctors, on_delete=models.DO_NOTHING, db_column='DocID', blank=True, null=True)  
+    timebooking = models.TimeField(db_column='Timebooking', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
--- appointments/views.py
+++ appointments/views.py
@@ -1,21 +1,57 @@
 from django.shortcuts import render, redirect
 from appointments.forms import AppointmentForm
 from appointments.models import Appointment
+from ministration.models import Ministration
+from doctors.models import Doctors
+from datetime import time
 
 # Create your views here.
 
-def create (request):
-    if request.method == "POST":  
-        form = AppointmentForm(request.POST)  
-        if form.is_valid():  
-            try:  
-                form.save()  
-                return redirect('/appointments')  
-            except:  
-                pass
-    else:  
-        form = AppointmentForm()
-    return render(request,"appointments/create.html",{'form': form})
+from django.shortcuts import render, redirect
+from .models import Appointment
+from ministration.models import Ministration
+
+def create(request):
+    if request.method == "POST":
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        datebooking = request.POST.get('datebooking')
+        note = request.POST.get('note')
+        timebooking_hour = request.POST.get('timebooking_hour')
+        timebooking_minute = request.POST.get('timebooking_minute')
+
+        # Get the selected Ministration and Doctor instances using their primary keys
+        serviceid_id = request.POST.get('serviceid')
+        serviceid = Ministration.objects.get(minisid=serviceid_id)
+
+        docid_id = request.POST.get('docid')
+        docid = Doctors.objects.get(doctorid=docid_id)
+
+        # Combine timebooking_hour and timebooking_minute into a valid time object
+        timebooking = time(int(timebooking_hour), int(timebooking_minute))
+
+        try:
+            Appointment.objects.create(
+                fullname=fullname,
+                email=email,
+                phone=phone,
+                datebooking=datebooking,
+                serviceid=serviceid,
+                docid=docid,
+                note=note,
+                timebooking=timebooking
+            )
+            return redirect('/appointments')  # Redirect to the appointments page after successful save
+        except:
+            pass
+
+    # Retrieve the related instances for rendering the dropdown
+    services = Ministration.objects.all()
+    doctors = Doctors.objects.all()
+
+    return render(request, "appointments/create.html", {'services': services, 'doctors': doctors})
+
 
 
 def index (request): 
@@ -23,6 +59,6 @@ def index (request):
     return render(request, "appointments/index.html",{'appointment': appointment})
 
 def clear (request,id):
-    appointment = Appointment.objects.get( appointmentid =id)
+    appointment = Appointment.objects.get( appid =id)
     appointment.delete()
     return redirect('/appointments')  
