2f2d1ff5cefcd43fb9579d2ab9fe5703b6ac383c
Marcussha <hathong.work@gmail.com>
2023-12-14 13:51:34 +0700

    fix bug

--- authrole/views.py
+++ authrole/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from authrole.forms import UserGroupsForm
 from authrole.models import AuthUserGroups
 from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from authrole.custom_context import user_is_admin
 
@@ -15,25 +16,24 @@ def index (request):
 @user_passes_test(lambda u: user_is_admin(u), login_url='login')
 def create(request):
     if request.method == "POST":
-        # If the request method is POST, it means the form is submitted        
         form = UserGroupsForm(request.POST)
         if form.is_valid():
             try:
                 form.save()
-                return redirect('/authrole')# Redirect to the appropriate URL after successful creation
-            except:
-                return HttpResponse("An error occurred during user group creation.") 
+                messages.success(request, 'User group created successfully.')
+                return redirect('/authrole') 
+            except Exception as e:
+                messages.error(request, f'An error occurred during user group creation: {e}')
+        else:
+            messages.error(request, 'Please correct the form errors.')
     else:
-        # If the request method is not POST, it means the user is accessing the page to fill out the form
         form = UserGroupsForm()
-        return render(request,'admin/authrole/create.html',{'form':form})
+
+    return render(request, 'admin/authrole/create.html', {'form': form})
 
 @user_passes_test(lambda u: user_is_admin(u), login_url='login')
 def delete(request, id):
     authroles = AuthUserGroups.objects.get(id=id)
     authroles.delete()
 
-    # Fetch the updated queryset after deletion
-    updated_authroles = AuthUserGroups.objects.all()
-
-    return render(request, 'admin/authrole/index.html', {'authroles': updated_authroles})
+    return redirect ('/authrole')
